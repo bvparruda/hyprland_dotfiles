864fa8c4706f8cf47802e84bf10795da15cbe9c5
Carlos Gabriel <csouzadsx@protonmail.com>
2026-08-16 00:42:11 -0300

    feat: add waybar config

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -18,6 +18,27 @@ setup_monitor("eDP-1", 1.25)
 
 
 
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+-- hl.on("hyprland.start", function () 
+--   hl.exec_cmd(terminal)
+--   hl.exec_cmd("nm-applet")
+--   hl.exec_cmd("waybar & hyprpaper & firefox")
+-- end)
+
+hl.exec_cmd("nm-applet")
+hl.exec_cmd("waybar")
+
+
+
+
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "dolphin"
